9e1d8e7271df7e8ca6bfd37f2275fc9ff1962710
Michael Bryant <e.kazuma@gmail.com>
2021-01-07 21:14:09 -0500

    Completed basic index builder

--- src/view-builder.js
+++ src/view-builder.js
@@ -17,13 +17,29 @@ function buildWordPages (dictionarySchema) {
 }
 
 function buildIndexPage (dictionarySchema) {
+  const byLetter = groupByLetter(dictionarySchema.dictionary)
+  const byTag = groupByTag(dictionarySchema.dictionary)
+
   return `<html>
   <head>
     <title>${dictionarySchema.name}</title>
   </head>
   <body>
-    Index
-    More coming soon!
+    <h1 class="dictionary-name">${dictionarySchema.name}</h1>
+    <p class="dictionary-description">${dictionarySchema.description}</p>
+    <details>
+      <summary>
+        <h2>Group by first letter</h2>
+      </summary>
+      ${buildGroupHTML(byLetter)}
+    </details>
+
+    <details>
+      <summary>
+        <h2>Group by tag</h2>
+      </summary>
+      ${buildGroupHTML(byTag)}
+    </details>
   </body>
 </html>
 `
@@ -47,7 +63,80 @@ function buildWordPage (word, entries, dictionaryName) {
 `
 }
 
+function groupByLetter (dict) {
+  return Object.keys(dict).reduce((byLetter, word) => {
+    const firstLetter = word.charAt(0).toLocaleUpperCase()
+    const letterList = byLetter[firstLetter]
 
+    if (letterList && letterList.length > 0) {
+      letterList.push({ word })
+    } else {
+      byLetter[firstLetter] = [{ word }]
+    }
+
+    return byLetter
+  }, {})
+}
+
+function groupByTag (dict) {
+  const byTag = {}
+
+  for(const word in dict) {
+    dict[word].forEach((entry, index) => {
+      entry.tags.forEach((tag) => {
+        const tagList = byTag[tag]
+
+        if(tagList && tagList.length > 0) {
+          tagList.push({ word, index })
+        } else {
+          byTag[tag] = [{ word, index }]
+        }
+      })
+    })
+  }
+
+  return byTag
+}
+
+function buildGroupHTML (group) {
+  const builtGroups = Object.entries(group)
+    .sort(([groupA], [groupB]) => groupA.localeCompare(groupB))
+    .map(buildGroup)
+    .join('\n')
+
+  return `<ul>
+  ${builtGroups}
+</ul>`
+}
+
+function buildGroup ([groupName, groupItems]) {
+  const builtItems = groupItems
+    .sort((a, b) => {
+      const wordCompare = a.word.localeCompare(b.word)
+
+      return wordCompare === 0
+        ? a.index - b.index
+        : wordCompare;
+    })
+    .map(buildGroupItem)
+    .join('\n')
+
+  return `<li>
+  <h3>${groupName}</h3>
+  <ul>
+    ${builtItems}
+  </ul>
+</li>`
+}
+
+function buildGroupItem (groupItem) {
+  const { word, index } = groupItem
+  if (index !== undefined) {
+    return `<li><a href="${word}#entry-${index}">${word}<sub>${index + 1}</sub></a></li>`
+  } else {
+    return `<li><a href="${word}">${word}</a></li>`
+  }
+}
 
 
 // ================================================================================================
